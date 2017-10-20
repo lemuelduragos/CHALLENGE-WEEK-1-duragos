b00571e877c641167b8b5df6a521225ce8420173
Lemuel <31605026+lemuelduragos@users.noreply.github.com>
2017-10-20 09:50:44 +0800

    sync to master

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+function autoPull() {
+
+	$commands = array(
+        'echo $PWD',
+        'whoami',
+        'sudo git pull',
+    );
+    foreach($commands AS $command){
+        shell_exec("$command 2>&1");
+    }
+
+
+    if
+}
+    
+?>
