666d55993c84d1605710f5be0c2e6e5e8b362e8b
Lemuel <31605026+lemuelduragos@users.noreply.github.com>
2017-10-20 10:48:12 +0800

    experiment on post

--- index.php
+++ index.php
@@ -17,5 +17,7 @@ autoPull();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo "Hello! Payload received";
+    $data = json_decode(file_get_contents('php://input'), true);
+	print_r($data);
 }
 ?>
